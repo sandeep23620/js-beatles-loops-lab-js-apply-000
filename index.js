3aaa7e4c7cde93ba118d5a6c75d44ac111a61824
Sandeep Sharma <sandeep.sharma1@blackrock.com>
2019-03-18 07:37:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-function theBeatlesPlay(){
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+function theBeatlesPlay(musicians[], instruments[]){
   
 }
